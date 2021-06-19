38bc9b279b1e1fbb2743b52680ee7f3ee6c05bdd
Nathan C <me@nathanc.tech>
2021-06-18 20:16:31 -0400

    bump version

--- civintegration/CHANGEMEconfig_civintegration.lua
+++ civintegration/CHANGEMEconfig_civintegration.lua
@@ -8,7 +8,7 @@
 ]]
 local config = {
     enabled = false,
-    configVersion = "1.0",
+    configVersion = "1.1",
     pluginName = "civintegration", -- name your plugin here
     pluginAuthor = "SonoranCAD", -- author
     requiresPlugins = {} -- required plugins for this plugin to work, separated by commas
